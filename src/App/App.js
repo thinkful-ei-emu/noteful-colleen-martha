c548978fd8029a98732c743d40858ffc30ec312a
Colleen <colleenhiggins87@gmail.com>
2019-07-03 12:00:54 -0700

    added delete button

--- src/App/App.js
+++ src/App/App.js
@@ -1,82 +1,79 @@
-import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import NoteListNav from '../NoteListNav/NoteListNav';
-import NotePageNav from '../NotePageNav/NotePageNav';
-import NoteListMain from '../NoteListMain/NoteListMain';
-import NotePageMain from '../NotePageMain/NotePageMain';
-import dummyStore from '../dummy-store';
-import NotefulContext from '../NotefulContext';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NoteListNav from "../NoteListNav/NoteListNav";
+import NotePageNav from "../NotePageNav/NotePageNav";
+import NoteListMain from "../NoteListMain/NoteListMain";
+import NotePageMain from "../NotePageMain/NotePageMain";
+import dummyStore from "../dummy-store";
+import NotefulContext from "../NotefulContext";
+import "./App.css";
 
 class App extends Component {
-    state = {
-        notes: [],
-        folders: []
-    };
+  state = {
+    notes: [],
+    folders: []
+  };
 
-    componentDidMount() {
-        // fake date loading from API call
-        setTimeout(() => this.setState(dummyStore), 600);
-    }
+  deleteNote = noteId => {
+    const newNotes = this.state.notes.filter(note => note.id !== noteId);
 
-    renderNavRoutes() {
-        return (
-            <>
-                {['/', '/folder/:folderId'].map(path => (
-                    <Route
-                        exact
-                        key={path}
-                        path={path}
-                        component={NoteListNav}
-                       
-                            />
-                        )
-                    
-                )}
-                <Route
-                    path="/note/:noteId"
-                    component={NotePageNav}
-                />
-                <Route path="/add-folder" component={NotePageNav} />
-                <Route path="/add-note" component={NotePageNav} />
-            </>
-        );
-    }
+    this.setState({
+      notes: newNotes
+    });
+  };
 
-    renderMainRoutes() {
-        return (
-            <>
-                {['/', '/folder/:folderId'].map(path => (
-                    <Route
-                        exact
-                        key={path}
-                        path={path}
-                        component= {NoteListMain}/>
-                ))}
-                <Route
-                    path="/note/:noteId"
-                    component={NotePageMain}/>
-            </>
-        );
-    }
+  componentDidMount() {
+    // fake date loading from API call
+    setTimeout(() => this.setState(dummyStore), 600);
+  }
 
-    render() {
-        return (
-            <NotefulContext.Provider value={{notes: this.state.notes, folders: this.state.folders }}>
-            <div className="App">
-                <nav className="App__nav">{this.renderNavRoutes()}</nav>
-                <header className="App__header">
-                    <h1>
-                        <Link to="/">Noteful</Link>{' '}
-                        <FontAwesomeIcon icon="check-double" />
-                    </h1>
-                </header>
-                <main className="App__main">{this.renderMainRoutes()}</main>
-            </div>
-            </NotefulContext.Provider>
-        );
-    }
+  renderNavRoutes() {
+    return (
+      <>
+        {["/", "/folder/:folderId"].map(path => (
+          <Route exact key={path} path={path} component={NoteListNav} />
+        ))}
+        <Route path="/note/:noteId" component={NotePageNav} />
+        <Route path="/add-folder" component={NotePageNav} />
+        <Route path="/add-note" component={NotePageNav} />
+      </>
+    );
+  }
+
+  renderMainRoutes() {
+    return (
+      <>
+        {["/", "/folder/:folderId"].map(path => (
+          <Route exact key={path} path={path} component={NoteListMain} />
+        ))}
+        <Route path="/note/:noteId" component={NotePageMain} />
+      </>
+    );
+  }
+
+  render() {
+    return (
+      <NotefulContext.Provider
+        value={{
+          notes: this.state.notes,
+          folders: this.state.folders,
+          deleteNote: this.deleteNote
+        }}
+      >
+        <div className="App">
+          <nav className="App__nav">{this.renderNavRoutes()}</nav>
+          <header className="App__header">
+            <h1>
+              <Link to="/">Noteful</Link>{" "}
+              <FontAwesomeIcon icon="check-double" />
+            </h1>
+          </header>
+          <main className="App__main">{this.renderMainRoutes()}</main>
+        </div>
+      </NotefulContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/Note/Note.js
+++ src/Note/Note.js
@@ -3,18 +3,20 @@ import NotefulContext from '../NotefulContext'
 import { Link } from 'react-router-dom'
 import { format } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 import './Note.css'
 
- export default function Note (props) {
+
+ export default class Note extends React.Component {
+  static contextType = NotefulContext 
+  render(){
   return (
     <div className='Note'>
       <h2 className='Note__title'>
-        <Link to={`/note/${props.id}`}>
-          {props.name}
+        <Link to={`/note/${this.props.id}`}>
+          {this.props.name}
         </Link>
       </h2>
-      <button className='Note__delete' type='button'>
+      <button className='Note__delete' type='button' onClick={() =>this.context.deleteNote(this.props.id)}>
         <FontAwesomeIcon icon='trash-alt' />
         {' '}
         remove
@@ -24,10 +26,11 @@ import './Note.css'
           Modified
           {' '}
           <span className='Date'>
-            {format(props.modified, 'Do MMM YYYY')}
+            {format(this.props.modified, 'Do MMM YYYY')}
           </span>
         </div>
       </div>
     </div>
   )
+}
 }
